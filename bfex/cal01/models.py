74e38545f24137233f5747bfa4acfe4725e53c4e
hecaiont <it8745@ocmobile.co.kr>
2020-03-19 21:01:01 +0900

    add a model, brew class

--- bfex/cal01/models.py
+++ bfex/cal01/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+# brew model
+class Brew(models.Model):
+    name = models.CharField(max_length=200)
+    brew_date = models.DateTimeField('date published')
+
