6a6340336d02f7e506a685ac25583e1487ce026d
hecaiont <it8745@ocmobile.co.kr>
2020-04-20 11:36:37 +0900

    model add some variable

--- bfex/cal01/models.py
+++ bfex/cal01/models.py
@@ -2,8 +2,33 @@ from django.db import models
 
 # brew model
 class Brew(models.Model):
-    name = models.CharField(max_length=200)
+    
+    brew_name = models.CharField(max_length=100)
     brew_date = models.DateTimeField('date published')
+    brew_description = models.CharField(max_length=200)
+    
+    # def ingredients
+    total_water = 
+    total_rice = 
+    total_stage = 
+    total_additive = 
 
     def __str__(self):
         return self.name
+
+
+
+class stage(models.Model):
+
+
+    stage_order = 
+    stage_type = 
+    stage_description = 
+    stage_date = 
+    
+    
+    # def ingredients    
+    rice = 
+    water = 
+    yeast = 
+    additive = 
